ab18b0a62f64e31f35e011aecbf89b34865069d6
Gabriel William Teñoso <gabrielwilliam0118@gmail.com>
2026-08-04 11:33:08 +0800

    feat: tab 2 new sale: auto update stock when new sale button clicked
     tab 1 add product: error handling

--- main.py
+++ main.py
@@ -157,12 +157,47 @@ class StainlessApp(QMainWindow):
     def add_prod_to_db(self):
         try:
             name = self.name_input.text().strip()
+            # checks if "name" is empty
             if not name:
                 raise ValueError("Product name cannot be empty!")
+            # checks if product name is or has a number
+            if any(char.isdigit() for char in name):
+                raise ValueError("Product name must not have any numbers!")
 
-            cost = float(self.cost_input.text())
-            sell = float(self.sell_input.text())
-            stock = int(self.stock_input.text())
+            # check "cost" if empty
+            if not self.cost_input.text().strip():
+                raise ValueError("Product cost cannot be empty!")
+            # checks if it is a float
+            try:
+                cost = float(self.cost_input.text())
+            except:
+                raise ValueError("Product cost must be a number!")
+            # check if less than or equal to zero
+            
+            if cost <= 0:
+                raise ValueError("Product cost must be greater than zero!")
+
+            # checks if sell is empty
+            if not self.sell_input.text().strip():
+                raise ValueError("Selling price cannot be empty!")
+            try:
+                sell = float(self.sell_input.text())
+            except:
+                raise ValueError("Selling price must be a number!")
+            # checks if sell is less than or equal to zero
+            if sell <= 0:
+                raise ValueError("Selling price must be greater than zero!")
+
+            # checks if stock input is empty
+            if not self.stock_input.text().strip():
+                raise ValueError("Stock cannot be empty!")
+            try:
+                stock = int(self.stock_input.text())
+            except:
+                raise ValueError("Stock must be a number!")
+            # checks if stock is less than zero
+            if stock < 0:
+                raise ValueError("Product stock cannot be less than zero!")
 
             
             database.add_product(name, cost, sell, stock)
@@ -182,7 +217,7 @@ class StainlessApp(QMainWindow):
             self.load_db_to_table()
 
         except ValueError as e:
-            QMessageBox.warning(self, "Input Error", "Please ensure all numbers are typed correctly and fields are not empty!")
+            QMessageBox.warning(self, "Input Error", str(e))
 
 
 
@@ -300,7 +335,7 @@ class StainlessApp(QMainWindow):
         database.update_product_stock(id, stock) 
 
 
-    
+
         database.new_sale(id, sold, quantity, cost, price)
 
 
@@ -309,6 +344,9 @@ class StainlessApp(QMainWindow):
         self.sold_to.clear()
         self.sale_quantity.clear()
         self.price_display_input.clear()
+
+        # updates the stock when adding a new sale once button hits
+        self.get_dropdown_data() 
 
         # sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
         # product_id INTEGER,
